cb82a727236f7e8500d9aabace7f3765166859bc
Dean <dean@orbonis.co.uk>
2020-08-11 21:03:24 +0100

    Initial Commit

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "semantic-ui-css/semantic.min.css"
+
+const element = document.createElement("div");
+element.id = "app";
+document.body.appendChild(element);
+
+ReactDOM.render(<div></div>, element);
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const { compilerOptions } = require("./tsconfig.json");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+const data = {
+    mode: "development",
+    entry: "./src/index.tsx",
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "index.js"
+    },
+    devtool: "source-map",
+    devServer: {
+        contentBase: path.join(__dirname, "build"),
+        port: 8080
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+        modules: [
+            path.resolve(__dirname, 'node_modules'),
+            path.resolve(__dirname, compilerOptions.baseUrl),
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                include: [
+                    path.resolve(__dirname, "src")
+                ],
+                loader: "ts-loader"
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+                loader: require.resolve("url-loader"),
+                options: {
+                    limit: 10000,
+                    name: "static/media/[name].[hash:8].[ext]",
+                },
+            },
+            {
+                test: [/\.eot$/, /\.ttf$/, /\.svg$/, /\.woff$/, /\.woff2$/],
+                loader: require.resolve("file-loader"),
+                options: {
+                    name: "static/media/[name].[hash:8].[ext]",
+                },
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: "Orbonis - Portfolio"
+        })
+    ]
+};
+
+module.exports = data;
